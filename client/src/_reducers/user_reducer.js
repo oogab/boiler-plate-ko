75572c1d838eceee3ccf283a56f9949056e28582
oogab <38171845+oogab@users.noreply.github.com>
2021-06-29 22:27:18 +0900

    react client update

---
+++ client/src/_reducers/user_reducer.js
@@ -0,0 +1,20 @@
+import { LOGIN_USER } from "../_actions/types"
+import { REGISTER_USER } from "../_actions/types"
+import { AUTH_USER } from "../_actions/types"
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function (state = {}, action) {
+    switch (action.type) {
+        case LOGIN_USER:
+            return { ...state, loginSuccess: action.payload}
+
+        case REGISTER_USER:
+            return { ...state, register: action.payload}
+
+        case AUTH_USER:
+            return { ...state, userData: action.payload}
+
+        default:
+            return state
+    }
+}
